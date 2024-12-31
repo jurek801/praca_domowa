437f20f8165ddadb7a2427c66149d72ae9731bf6
gstencel <gstencel@mat.umk.pl>
2024-12-31 02:52:32 +0100

    Zaktualizowano menu dodając opcję Silnia

--- projekt1.cpp
+++ projekt1.cpp
@@ -18,8 +18,13 @@ int main() {
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+	cout << "1. Silnia" << endl;
         cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
+
+	if(wyjscie == 1){
+		cout << a << "! = " << silnia(a) << endl;
+	}
     } while(wyjscie != 0);
     return 0;
 }
